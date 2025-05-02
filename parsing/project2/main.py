50c132a5919b2067472b89b4dee30a9af0313c8f
alipy-dw <ebaka646@gmail.com>
2025-05-02 08:23:34 +0600

    commit

--- parsing/project2/main.py
+++ parsing/project2/main.py
@@ -11,18 +11,11 @@ date_of_news = soup.find_all(class_="lineDate")
 news_time = soup.find_all('div', class_="time")
 news_title = soup.find('div', class_="row lineNews").find_all('div', class_="title")
 news_list = []
-for date, time, title in  zip(date_of_news,news_time, news_title):
-    if not date in date_of_news: #тут я хочу что бы цикл шел даже если нету даты в date of news, как мне сделать так
-        news_list.append({
-            'TIME':time.text,
-            "TITLE":title.text
-        })
-    else:
-        news_list.append({
-            "DATE":date.text,
-            'TIME':time.text,
-            "TITLE":title.text
-        })
+for time, title in  zip(news_time, news_title):
+    news_list.append({
+        'TIME':time.text,
+        "TITLE":title.text
+    })
 with open('/home/hello/Рабочий стол/practice/parsing/project2/news_data.json', 'w') as file:
     json.dump(news_list, file, ensure_ascii=False, indent=4)
 
